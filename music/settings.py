bd65c319aa7adf016161c8fd6aca93f3a132a197
Ann.Ke <ann.ke@cloud-interactive.com>
2021-03-09 12:09:16 +0800

    1. set test env

--- music/settings.py
+++ music/settings.py
@@ -22,8 +22,9 @@ env = environ.Env(
 # reading .env file
 environ.Env.read_env()
 
-ENV = os.getenv('ENV').lower()
+ENV = os.environ.get('ENV').lower()
 
+print('TEST_ENV is =>', os.environ.get('TEST_ENV'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
